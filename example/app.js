fed5cb15e5c2f52bf342a9517149fbfad2ee774c
wang zuo <wzuoadjusted@gmail.com>
2015-05-22 17:14:15 +0800

    a better example

--- example/app.js
+++ example/app.js
@@ -2,12 +2,23 @@ var React = require('react');
 var Frame = require('../lib/frame');
 
 var App = React.createClass({
+  getInitialState() {
+    return {text: 'Parturient Ipsum Cursus Purus Justo'};
+  },
+
+  handleChange(e) {
+    this.setState({text: e.target.value});
+  },
+
   render() {
     return (
       <div>
-        <div className="title">Parturient Ipsum Cursus Purus Justo</div>
-        <Frame styleSheets={['/example/frame.css']}>
-          <div className="title">Parturient Ipsum Cursus Purus Justo</div>
+        <textarea style={{width: 400}} value={this.state.text} onChange={this.handleChange}/>
+        <div className="title">{this.state.text}</div>
+        <Frame className="iframe" id="iframe" styleSheets={['frame.css']}>
+        <div>
+          <div className="title">{this.state.text}</div>
+        </div>
         </Frame>
       </div>
     )
